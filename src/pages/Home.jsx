557f95f3095f22cf4b61ead5a93cb3f746906e1b
Kirill Zhukau <67194208+kotikkir9@users.noreply.github.com>
2022-06-19 01:53:32 +0200

    created cool 'new session' button for touch screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,36 @@ function Home() {
     const navigate = useNavigate();
 
 
+    const handleTouchStart = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const posY = e.touches[0].pageY - rect.top;
+        const posX = e.touches[0].pageX - rect.left;
+
+        const fill = e.currentTarget.firstChild;
+        fill.style.left = `${posX}px`
+        fill.style.top = `${posY}px`;
+
+        e.currentTarget.classList.add(classes.active);
+    }
+
+    const handleTouchEnd = (e) => {
+        e.preventDefault();
+        e.currentTarget.classList.remove(classes.active);
+
+        const rect = e.currentTarget.getBoundingClientRect();
+        const centerX = rect.x + (rect.width / 2);
+        const centerY = rect.y + (rect.height / 2);
+
+        const touchX = e.changedTouches[0].clientX;
+        const touchY = e.changedTouches[0].clientY;
+
+        const diff = Math.sqrt(Math.pow(Math.abs(touchX - centerX), 2) + Math.pow(Math.abs(touchY - centerY), 2));
+
+        if(diff <= (rect.width / 2)) {
+            navigate('/session/new', { replace: true });
+        }
+    }
+
     return (
         <div className={classes.container}>
             <Header>
@@ -29,7 +59,9 @@ function Home() {
                         <Session text={9} />
                         <Session text={10} />
                     </div>
-                    <div className={classes["add-btn"]} onClick={() => navigate('/session/new', { replace: true })}></div>
+                    <div className={classes["add-btn"]} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} >
+                        <div className={classes['btn-fill']}></div>
+                    </div>
                 </section>
             </main>
         </div>
